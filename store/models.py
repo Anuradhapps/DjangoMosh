a6c60c9677d40088af03ee8c459a846ee30d6099
Anuradhapps <162093163+Anuradhapps@users.noreply.github.com>
2026-03-27 08:40:06 +0530

    done

--- store/models.py
+++ store/models.py
@@ -11,8 +11,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -34,10 +35,15 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
+    class Meta:
+        db_table  = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255, null=True)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE , primary_key=True) 
 
 class Order(models.Model):
