3bf80480be0c55ab108302dd56d9350203f8c22b
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2026-03-31 16:18:01 +0530

    Refactor database settings and update migrations
    - Changed database engine from SQLite to MySQL in settings.py.
    - Added new migrations to alter primary key fields to BigAutoField for models in the store and tags apps.
    - Removed unnecessary indexes and altered model tables for the Customer model.
    - Added SQL migration to insert a default collection into the store_collection table.
    - Updated templates and views to reflect changes in customer data retrieval.
    - Added requirements.txt for project dependencies.

--- djangomosh/settings.py
+++ djangomosh/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for djangomosh project.
 
@@ -85,10 +86,20 @@ WSGI_APPLICATION = 'djangomosh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_test',        # Your database name
+        'USER': 'root',       # MySQL username
+        'PASSWORD': 'root',       # MySQL password
+        'HOST': 'localhost',  # Usually localhost
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from store.models import Customer
 
 def say_hello(request):
-    x=1
-    y=2
+    try:
+        customer = Customer.objects.get(pk=1400)
+    except ObjectDoesNotExist:
+        customer = None
+
     return render(request, 'hello.html',{
-        'name':'Drasha'
+        'customer': customer.first_name if customer else None
+    })
+
+
+    return render(request, 'hello.html',{
+        'customer': customer.first_name
     })
