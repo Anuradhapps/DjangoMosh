d9dbd96df6713cdb53363ed580852f52a98a5592
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2026-04-07 22:28:45 +0530

    Refactor templates and styles: update header layout, enhance main content styling, and modify hello.html to display a simple greeting. Add main.js for future JavaScript functionality.

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, OrderItem, Order
+from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+from django.db.models import Q, F, Value
 
 
 def say_hello(request):
-    product = Product.objects.filter(unit_price__gt=450)
 
-    return render(request, "hello.html", {"product": product})
+    querryset = Product.objects.annotate(isnew=Value(True))
+    return render(request, "hello.html", {"products": list(querryset)})
