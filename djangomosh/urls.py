39b0f5c2f88a6b3c485c2b76c393b4fc87deeb19
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2026-04-10 16:04:39 +0530

    Add Django REST framework support: update Pipfile and Pipfile.lock, include rest_framework in installed apps, and create product list and detail views with corresponding URLs.

--- djangomosh/urls.py
+++ djangomosh/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("", include("playground.urls")),
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
+    path("store/", include("store.urls")),
     path("__debug__/", include(debug_toolbar.urls)),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
